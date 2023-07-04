85ef1db1035676c1ff527a2df11d9482ccce29c7
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-07-04 12:37:04 +0200

    add ex3

---
+++ src/main/java/com/gianmarco/streamslambdas/Exercise3.java
@@ -0,0 +1,19 @@
+// How could you create a stream of cars without first explicitly creating a collection of cars?
+
+package com.gianmarco.streamslambdas;
+
+import java.time.Year;
+import java.util.stream.Stream;
+
+public class Exercise3 {
+
+//    public static void main(String[] args) {
+//        Stream.of(
+//            new Exercise2.Car("VW", "Polo", Year.of(2019))),
+//            new Exercise2.Car("Ford", "Fiesta", Year.of(2023)),
+//            new Exercise2.Car("Tesla", "Y", Year.of(2017)),
+//            new Exercise2.Car("Toyota", "Yaris", Year.of(2022))
+//        )
+//
+//    }
+}
